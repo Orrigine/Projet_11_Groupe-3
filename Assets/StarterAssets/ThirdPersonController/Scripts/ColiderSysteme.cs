b4a90dd5b051270006bcf6f6d96cd6dc8ed3487d
AuraXelus <come.santin@gmail.com>
2023-04-13 12:30:17 +0200

    DernierDernierDernier

--- Assets/StarterAssets/ThirdPersonController/Scripts/ColiderSysteme.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ColiderSysteme.cs
@@ -7,7 +7,7 @@ public class ColiderSysteme : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(this.gameObject, 10);
     }
 
     // Update is called once per frame
